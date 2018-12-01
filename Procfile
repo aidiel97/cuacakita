fc8d697f911253150e3e7b409b688202a8fa7704
aidiel_r <aidielrachman@gmail.com>
2018-12-01 07:54:19 +0700

    coba timer

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+worker : python3 tweet_poster.py
 web: python3 indexFull.py
--- tweet_poster.py
+++ tweet_poster.py
@@ -1,5 +1,6 @@
 import tweepy
 from RequestAPI import RequestAPI
+import time
 
 class tweet_poster:
 	def post(self, sentence):
@@ -21,3 +22,28 @@ class tweet_poster:
 		tweepyapi.update_status(sentence)
 		print("tweet berhasil di post :", sentence)
 
+	def postm(self):
+		consumer_key = "k2HFUcJA2CMcb2JwWnFYXmwGs"
+		consumer_secret = "ZnedSsVh8nAJrhkwMbyITCJsTCP4D4a8VO2zhr7Qj5kcya1UN1"
+
+		access_token = "1019130150405828610-tjzmuDjAFXBvwftL6ugevFtd1vPhys"
+		access_token_secret = "e7zUjaCimnYKQtlrRu75ynXupUQu4NkTrQmN3wBuM7YPO"
+
+		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+		auth.set_access_token(access_token, access_token_secret)
+
+		INTERVAL = 60
+		tweepyapi = tweepy.API(auth)
+		# text_gen = Text_generator()
+
+		# data = text_gen.getData()
+		# sentence = text_gen.generator(data)
+		a = 35
+		while True:
+			tweepyapi.update_status(a)
+			print("tweet berhasil di post :", a)
+			a+=1
+			time.sleep(INTERVAL)
+
+p = tweet_poster()
+p.postm()
