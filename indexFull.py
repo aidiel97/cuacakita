ed9da3da9559e7daf88f33fda82f29b9f64ee266
aidiel_r <aidielrachman@gmail.com>
2018-12-03 18:04:58 +0700

    Tes

--- indexFull.py
+++ indexFull.py
@@ -20,26 +20,26 @@ def allowed_file(filename):
 def main():
     return render_template('index.html')
 
-@app.route("/post",methods=["POST"])
-def post():
-	consumer_key = "k2HFUcJA2CMcb2JwWnFYXmwGs"
-	consumer_secret = "ZnedSsVh8nAJrhkwMbyITCJsTCP4D4a8VO2zhr7Qj5kcya1UN1"
+# @app.route("/post",methods=["POST"])
+# def post():
+# 	consumer_key = "k2HFUcJA2CMcb2JwWnFYXmwGs"
+# 	consumer_secret = "ZnedSsVh8nAJrhkwMbyITCJsTCP4D4a8VO2zhr7Qj5kcya1UN1"
 
-	access_token = "1019130150405828610-tjzmuDjAFXBvwftL6ugevFtd1vPhys"
-	access_token_secret = "e7zUjaCimnYKQtlrRu75ynXupUQu4NkTrQmN3wBuM7YPO"
+# 	access_token = "1019130150405828610-tjzmuDjAFXBvwftL6ugevFtd1vPhys"
+# 	access_token_secret = "e7zUjaCimnYKQtlrRu75ynXupUQu4NkTrQmN3wBuM7YPO"
 
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_token, access_token_secret)
+# 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# 	auth.set_access_token(access_token, access_token_secret)
 
-	tweepyapi = tweepy.API(auth)
-	text_gen = Text_generator()
+# 	tweepyapi = tweepy.API(auth)
+# 	text_gen = Text_generator()
 
-	data = text_gen.getData()
-	sentence = text_gen.generator(data)
+# 	data = text_gen.getData()
+# 	sentence = text_gen.generator(data)
 	
-	tweepyapi.update_status(sentence)
-	print("tweet berhasil di post :", sentence)
-	return "berhasil"
+# 	tweepyapi.update_status(sentence)
+# 	print("tweet berhasil di post :", sentence)
+# 	return "berhasil"
 
 @app.route("/get_data", methods=["GET"])
 def get_data():
