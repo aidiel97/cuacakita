a512d8947cb27e46c5b2130010fd43045adfe7dd
aidiel_r <aidielrachman@gmail.com>
2019-04-01 18:17:03 +0700

    perbaikan reply

--- clock.py
+++ clock.py
@@ -1,10 +1,76 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import tweepy
+import time
+import re
+from translator import Translate
+
+print('Reply BOT!', flush=True)
+
+consumer_key = "k2HFUcJA2CMcb2JwWnFYXmwGs"
+consumer_secret = "ZnedSsVh8nAJrhkwMbyITCJsTCP4D4a8VO2zhr7Qj5kcya1UN1"
+
+access_token = "1019130150405828610-tjzmuDjAFXBvwftL6ugevFtd1vPhys"
+access_token_secret = "e7zUjaCimnYKQtlrRu75ynXupUQu4NkTrQmN3wBuM7YPO"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# mentions = api.mentions_timeline()
+
+# for mention in mentions:
+# 	print( str(mention.id) +" - "+ mention.text)
+
+FILE_NAME = 'last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name): #melihat idtweet terakhir yang distore, mencegah, tweet dibalas duakali
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name): #setelah dibaca tweet mention yg terakhir, maka idtweet akan dicatat di file 'last_seen_id.txt'
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def reply_to_tweets():
+    print('retrieving and replying to tweets...', flush=True)
+    # id last seen testing : 1112619279025725441
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    mentions = api.mentions_timeline(
+                        last_seen_id,
+                        tweet_mode='extended')
+    for mention in reversed(mentions): #supaya membaca perulangannya terbalik..
+    #biasanya list mention di timline dibaca dari yg terakhir dahulu, supaya terurut, maka dibalik
+        print(str(mention.id) + ' - ' + mention.full_text, flush=True)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+
+        tweet = mention.full_text
+        city = re.findall(r"#(\w+)", tweet)
+
+        translate = Translate()
+        ans = translate.kamusDaerah(city)
+        print(ans)
+
+        # if '#hai' in mention.full_text.lower():
+        if ans != "no":
+            print('menemukan #hai!', flush=True)
+            print('respond tweet...', flush=True)
+            api.update_status('Hai! @' + mention.user.screen_name + 
+            	" " + ans + " itu nama daerah di Indonesia kan? ramalan cuacanya menyusul ya :)", mention.id)
+
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=0.25)
 def timed_job():
     print('This job is run every 0.25 minutes.')
+    reply_to_tweets()
+
 
 # @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 # def scheduled_job():
