2e79162b0e57cdb549297c70529e69bcb03acf07
aidiel_r <aidielrachman@gmail.com>
2019-04-26 15:35:41 +0700

    test clock

--- indexFull.py
+++ indexFull.py
@@ -18,36 +18,6 @@ def allowed_file(filename):
 
 @app.route("/")
 def main():
-	consumer_key = "k2HFUcJA2CMcb2JwWnFYXmwGs"
-	consumer_secret = "ZnedSsVh8nAJrhkwMbyITCJsTCP4D4a8VO2zhr7Qj5kcya1UN1"
-
-	access_token = "1019130150405828610-tjzmuDjAFXBvwftL6ugevFtd1vPhys"
-	access_token_secret = "e7zUjaCimnYKQtlrRu75ynXupUQu4NkTrQmN3wBuM7YPO"
-
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_token, access_token_secret)
-
-	tweepyapi = tweepy.API(auth)
-
-	#memastikan mention ga dibalas dua kali
-	file_name = 'last_seen_id.txt'
-
-	f_read = open(file_name, 'r')
-	last_seen_id = int(f_read.read().strip())
-	f_read.close()
-
-	mentions = tweepyapi.mentions_timeline(last_seen_id, tweet_mode='extended')
-
-	if len(mentions) != 0 :
-		last_seen_id_w = mentions[0].id
-
-		f_write = open(file_name, 'w')
-		f_write.write(str(last_seen_id_w))
-		f_write.close()
-
-		print("last seen : ",last_seen_id_w)
-
-
 	return render_template('index.html')
 
 @app.route("/post")
