5db8e225d5a85ad0d51d6e7837b8df14cf2d2a3c
aidiel_r <aidielrachman@gmail.com>
2019-07-29 10:37:17 +0700

    Perbaikan token

--- tweet_poster.py
+++ tweet_poster.py
@@ -3,11 +3,11 @@ from RequestAPI import RequestAPI
 
 class tweet_poster:
 	def post(self, sentence):
-		consumer_key = "k2HFUcJA2CMcb2JwWnFYXmwGs"
-		consumer_secret = "ZnedSsVh8nAJrhkwMbyITCJsTCP4D4a8VO2zhr7Qj5kcya1UN1"
+		consumer_key = "N343ic9LxR6lIdZVaa2R1P0Gm"
+		consumer_secret = "oKPXVw45vawk6H6MfEgabgmPENUervO7rV1lk3Qoy1JPQ1ZEMG"
 
-		access_token = "1019130150405828610-tjzmuDjAFXBvwftL6ugevFtd1vPhys"
-		access_token_secret = "e7zUjaCimnYKQtlrRu75ynXupUQu4NkTrQmN3wBuM7YPO"
+		access_token = "1019130150405828610-fare5LY8Meyj715ijCq5SzIlnDEcDb"
+		access_token_secret = "IPDWmVFtiv0S7bYESYQF4AFpNwQ9qtEXUOcF1gtHq9r3o"
 
 		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 		auth.set_access_token(access_token, access_token_secret)
